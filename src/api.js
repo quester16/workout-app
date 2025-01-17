55cb0c7288c2aeebff39852af17275d8dc1f1298
saidburhon <saidburhoncoder@gmail.com>
2025-01-17 10:59:45 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 import { TOKEN } from './app.constans.js'
 
 export const caxios = axios.create({
-	baseURL: 'http://localhost:5555/api',
+	baseURL: 'https://wa-backend-xi.vercel.app/api',
 	headers: {
 		'Content-Type': 'application/json',
 		Authorization: `Bearer ${Cookies.get(TOKEN)}`
